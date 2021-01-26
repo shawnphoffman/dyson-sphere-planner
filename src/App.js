1d6bcb73b022faf34b817924840f330bf08d3dee
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2021-01-26 08:50:29 -0800

    Stub in coming soon display

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import { styled } from '@linaria/react'
+import colors from './utils/colors'
 
 const Title = styled.h1`
-	color: darkred;
-`
-
-const Description = styled.div`
-	font-size: ${props => props.size}px;
+	color: ${colors.white};
+	font-family: 'Teko', sans-serif;
+	font-weight: 300;
+	font-size: 6rem;
+	margin-top: 1em;
+	text-shadow: 1px 2px ${colors.orange};
+	letter-spacing: 0.05em;
 `
 
 function App() {
 	return (
 		<div>
-			<Title>Test</Title>
-			<Description size={12}>This is a description.</Description>
+			<Title>Coming Soon...</Title>
 		</div>
 	)
 }
---
+++ src/utils/colors.js
@@ -0,0 +1,7 @@
+const colors = {
+	white: 'white',
+	orange: '#ffb94c',
+	deepBlue: '#000112',
+}
+
+export default colors
