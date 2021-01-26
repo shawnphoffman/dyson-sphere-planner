1943fb9ef758d35296a4723d21a9f9362dcff4fd
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2021-01-26 14:46:51 -0800

    temp

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,47 @@
+const path = require('path')
 const { loaderByName, getLoader } = require('@craco/craco')
 const transformBabelLoader = require('./config/transformBabelLoader')
+var SpritesmithPlugin = require('webpack-spritesmith')
 
 module.exports = {
 	webpack: {
 		configure: webpackConfig => {
-			// console.log(JSON.stringify(webpackConfig.module.rules, null, 2))
+			//
+			// console.log(JSON.stringify(webpackConfig.resolve, null, 2))
 			// throw 'test'
+
+			//
 			const lm = getLoader(webpackConfig, loaderByName('babel-loader'))
 			const loader = lm.match.loader
 			webpackConfig.module.rules[1].oneOf[2] = transformBabelLoader(loader)
+
+			//
+			webpackConfig.resolve.modules.push('webpack-spritesmith')
+			webpackConfig.plugins.push(
+				new SpritesmithPlugin({
+					src: {
+						cwd: path.resolve(__dirname, 'src/images'),
+						glob: '*.png',
+					},
+					target: {
+						image: path.resolve(__dirname, 'src/.spritesmith-generated/sprite.png'),
+						css: [
+							path.resolve(__dirname, 'src/.spritesmith-generated/sprite.css'),
+							[
+								path.resolve(__dirname, 'src/.spritesmith-generated/sprite.json'),
+								{
+									format: 'json',
+								},
+							],
+						],
+					},
+					apiOptions: {
+						cssImageRef: './sprite.png',
+					},
+				})
+			)
+
+			//
 			return webpackConfig
 		},
 	},
--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { styled } from '@linaria/react'
 import colors from './utils/colors'
 
+import './.spritesmith-generated/sprite.css'
+import sprites from './.spritesmith-generated/sprite.json'
+
+const SpriteWrapper = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`
+
 const Title = styled.h1`
 	color: ${colors.white};
 	font-family: 'Teko', sans-serif;
@@ -10,12 +18,18 @@ const Title = styled.h1`
 	margin-top: 1em;
 	text-shadow: 1px 2px ${colors.orange};
 	letter-spacing: 0.05em;
+	text-align: center;
 `
 
 function App() {
 	return (
 		<div>
 			<Title>Coming Soon...</Title>
+			<SpriteWrapper>
+				{Object.keys(sprites).map(s => (
+					<div className={`icon-${s}`} key={s} />
+				))}
+			</SpriteWrapper>
 		</div>
 	)
 }
